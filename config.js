a4f5da1bf76706f7a93d4fd5e8db09667a3c136a
Danzmd <107999522+Danzmd@users.noreply.github.com>
2022-06-24 14:00:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,8 @@ global.APIKeys = { // APIKey Here
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 const spack = fs.readFileSync("lib/exif.json")
-const stickerpack = JSON.parse(spack)
+const stickerpack = JS
+ON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = '@Danzz_bot'
   var sticker_author = 'Danzz bot'
