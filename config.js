e47433b3861ea6b7354d583cc2a3e5ca23ff9fef
Danzmd <107999522+Danzmd@users.noreply.github.com>
2022-06-29 09:47:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ let chalk = require('chalk')
 const danzz = JSON.parse(fs.readFileSync('./lib/aine1.json'))
 
 global.owner = [
-  [danzz.opw],
-  ['62895342755780', 'Owner', true]
+  [xyra.opw],
+  ['6282197940309', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@Ryu_bot'
-  var sticker_author = 'Ryu bot'
+  var sticker_name = '@xyra_bot'
+  var sticker_author = 'xyra bot'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Ryu bot'
+global.wm = '© xyra bot'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
